fe3bbb345a03afd205af5d985cda53c4be3c906c
Yaniv Kalsky <yanivka@gmail.com>
2018-06-26 11:40:01 -0700

    update sh to support installation on ubuntu

--- applications/psql/psql.sh
+++ applications/psql/psql.sh
@@ -11,20 +11,24 @@
 # chmod 777 install_postgresql.sh
 # ./install_postgresql.sh
 ###############################################
-echo "*****************************************"
-echo " Installing PostgreSQL"
-echo "*****************************************"
-sudo yum update
-sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
-sudo service postgresql initdb
+#echo "*****************************************"
+#echo " Installing PostgreSQL"
+#echo "*****************************************"
+#sudo yum update
+#sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
+#sudo service postgresql initdb
 # Use MD5 Authentication
-sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba.conf
+#sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /var/lib/pgsql9/data/pg_hba.conf
 #start
-sudo /sbin/chkconfig --levels 235 postgresql on
-sudo service postgresql start
+#sudo /sbin/chkconfig --levels 235 postgresql on
+#sudo service postgresql start
 
+sudo apt update
+sudo apt install postgresql postgresql-contrib -y
 
-psql -U postgres
+sudo -i -u postgres
+psql
+
 ALTER USER postgres WITH PASSWORD '$DB_PASS';
 CREATE DATABASE $DB_NAME;
 \q
