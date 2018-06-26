e7d74c0f0b4bd61501d9cf4a2a66bde2e57a998b
Yaniv Kalsky <yanivka@gmail.com>
2018-06-26 16:05:12 -0700

    more echos

--- applications/psql/psql.sh
+++ applications/psql/psql.sh
@@ -23,17 +23,29 @@
 #sudo /sbin/chkconfig --levels 235 postgresql on
 #sudo service postgresql start
 
+echo "****************************************************************"
+echo "Installing PostgreSQL"
+echo "****************************************************************"
 sudo apt update
 sudo apt install postgresql postgresql-contrib -y
 
+echo "****************************************************************"
+echo "Setting user"
+echo "****************************************************************"
 sudo -u postgres createuser -s $(whoami); createdb $(whoami)
 sudo -u postgres -c "ALTER USER postgres WITH PASSWORD '$DB_PASS';"
 
-#Use MD5 Authentication
+echo "****************************************************************"
+echo "Use MD5 Authentication"
+echo "****************************************************************"
 sudo sed -i.bak -e 's/ident$/md5/' -e 's/peer$/md5/' /etc/postgresql/9.5/main/pg_hba.conf
 sudo service postgresql restart
 
-#sudo su postgres
-sudo PGPASSWORD=$DB_PASS psql -U postgres -c "CREATE DATABASE $DB_NAME";
 
-sudo PGPASSWORD=$DB_PASS psql -U postgres -d $DB_NAME -c "CREATE TABLE tickets(id serial primary key,author VARCHAR(15),subject VARCHAR(50),issue VARCHAR(255),chatUrl VARCHAR(255),createdAt VARCHAR(100),archive BOOLEAN,status BOOLEAN)"
+echo "****************************************************************"
+echo "Create db and table"
+echo "****************************************************************"
+export PGPASSWORD=$DB_PASS
+sudo psql -U postgres -c "CREATE DATABASE $DB_NAME";
+
+sudo psql -U postgres -d $DB_NAME -c "CREATE TABLE tickets(id serial primary key,author VARCHAR(15),subject VARCHAR(50),issue VARCHAR(255),chatUrl VARCHAR(255),createdAt VARCHAR(100),archive BOOLEAN,status BOOLEAN)"
