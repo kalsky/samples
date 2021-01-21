264a798e6c0d0bddef11c0c411ce2c2340f382e7
kalsky <yanivka@gmail.com>
2021-01-21 10:35:55 -0800

    Add/Update files for deploying kalsky/django-dashboard-black

--- applications/ddb-app/init.sh
+++ applications/ddb-app/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get -y install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install virtualenv
 sudo mkdir /var/www
 sudo mkdir /var/www/ddb
 git clone https://github.com/kalsky/django-dashboard-black --depth 1 --branch=master /var/www/ddb
@@ -6,5 +10,5 @@ cd /var/www/ddb
 virtualenv env
 source env/bin/activate
 pip3 install -r requirements
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
