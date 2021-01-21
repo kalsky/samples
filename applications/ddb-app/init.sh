316a33be2fa21829048248975d1f5a37b7a81a65
kalsky <yanivka@gmail.com>
2021-01-21 11:40:23 -0800

    Add/Update files for deploying kalsky/django-dashboard-black

--- applications/ddb-app/init.sh
+++ applications/ddb-app/init.sh
@@ -7,7 +7,9 @@ sudo mkdir /var/www
 sudo mkdir /var/www/ddb
 git clone https://github.com/kalsky/django-dashboard-black --depth 1 --branch=master /var/www/ddb
 cd /var/www/ddb
+echo "ALLOWED_HOSTS.append('*')" | sudo tee -a core/settings.py
 sudo sqlite3 db.sqlite3 "VACUUM;"
+sudo chmod 755 db.sqlite3
 sudo pip3 install -r requirements.txt
 sudo python3 manage.py collectstatic --noinput
 sudo python3 manage.py makemigrations
