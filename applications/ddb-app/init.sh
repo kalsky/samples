a1aee8a0e3b5e7ed66b7b1f4a781ac9526398f80
kalsky <yanivka@gmail.com>
2021-01-21 09:27:13 -0800

    Add/Update files for deploying kalsky/django-dashboard-black

--- applications/ddb-app/init.sh
+++ applications/ddb-app/init.sh
@@ -1,20 +1,9 @@
-'#!/bin/bash
-
-  mkdir ~/ddb
-
-  cd ~/ddb
-
-  git clone https://github.com/kalsky/django-dashboard-black --depth 1 --branch=master
-  ~/ddb
-
-  virtualenv env
-
-  source env/bin/activate
-
-  pip3 install -r requirements
-
-  python manage.py makemigrations
-
-  python manage.py migrate
-
-  '
+#!/bin/bash
+mkdir ~/ddb
+cd ~/ddb
+git clone https://github.com/kalsky/django-dashboard-black --depth 1 --branch=master ~/ddb
+virtualenv env
+source env/bin/activate
+pip3 install -r requirements
+python manage.py makemigrations
+python manage.py migrate
--- applications/ddb-app/start.sh
+++ applications/ddb-app/start.sh
@@ -1,5 +1,2 @@
-'#!/bin/bash
-
-  sudo python manage.py runserver 80
-
-  '
+#!/bin/bash
+sudo python manage.py runserver 80
